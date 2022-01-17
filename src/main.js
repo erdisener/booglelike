a045b12320a40ed3839df99c92f00d617a0a341a
Erdi Sener <erdi.sener@sisu9.com>
2022-01-17 16:25:31 +0300

    authentication, store işlemleri

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router';
+import store from './store';
+import '@/assets/style.css';
+import { appAxios } from '@/utils/appAxios';
 
-createApp(App).mount('#app')
+import appHeader from "@/components/Main/appHeader";
+import appBookmarkList from "@/components/Main/appBookmarkList";
+
+const app = createApp(App);
+app.component("AppHeader", appHeader);
+app.component("appBookmarkList", appBookmarkList);
+app.use(router);
+app.use(store);
+app.config.globalProperties.$appAxios = appAxios;
+app.mount('#app')
